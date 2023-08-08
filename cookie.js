20b2705c08be234ae94d387cfd38eb89c6debed2
hackerAbdul <abdulkadir.q12@gmail.com>
2023-08-08 13:54:40 +0100

    adding comments and renaming variables

--- cookie.js
+++ cookie.js
@@ -1,35 +1,44 @@
-const img = document.getElementById("img");
+//this is where we declare our variables to be referenced throughout the page
 const body = document.body
+const img = document.getElementById("img");
 const counter = document.getElementById("counter")
-const btn = document.querySelector('button')
-const btn2 = document.getElementById('button')
+const restartButton = document.querySelector('button')
+const multiplyButton = document.getElementById('button')
 const theme = document.getElementById('theme')
 const icon = document.querySelector('i')
+
+//start by setting our counter at 0
 let num = 0
 counter.innerText = num + " Cookies"
 
+
+//once image is pressed it fires off this event listener that will update the counter based on number of clicks
 img.addEventListener("click", function(e){
     num++
     counter.innerText = `${num} Cookies`
 
+    //this condition checks if the counter is above 5 it will unhide the restart and *2 button
     if (num>5){
-        btn.removeAttribute("hidden")
-        btn2.removeAttribute("hidden")
+        restartButton.removeAttribute("hidden")
+        multiplyButton.removeAttribute("hidden")
     }
 })
 
-btn.addEventListener("click", function(e){
+//once the restart button is pressed it will fire off the event listener that will restart the counter and hide both buttons 
+restartButton.addEventListener("click", function(e){
     num = 0
     counter.innerText = `${num} Cookies`
-    btn.setAttribute("hidden", true)
-    btn2.setAttribute("hidden", true)
+    restartButton.setAttribute("hidden", true)
+    multiplyButton.setAttribute("hidden", true)
 })
 
-btn2.addEventListener("click", function(e){
+//the multiply button will multiply whatever value stored in the counter by 2
+multiplyButton.addEventListener("click", function(e){
     num *=2
     counter.innerText = `${num} Cookies`
 })
 
+//theme button will change the theme from light and darkmode based on what is set in the darkmode class in css
 theme.addEventListener("click", function(e){
     body.classList.toggle("darkmode")
     if (icon.classList.contains("fa-regular")){
