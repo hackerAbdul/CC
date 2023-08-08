591e453e0d02b72733bead1d389be4ed98329ae2
hackerAbdul <abdulkadir.q12@gmail.com>
2023-08-08 12:49:10 +0100

    updating style keeping it simple, along with animation + transition on image

--- cookie.js
+++ cookie.js
@@ -3,24 +3,28 @@ const body = document.body
 const counter = document.getElementById("counter")
 const btn = document.querySelector('button')
 const btn2 = document.getElementById('button')
-counter.innerText = 0
+let num = 0
+counter.innerText = num + " Cookies"
 
 img.addEventListener("click", function(e){
-    counter.innerText ++;
+    num++
+    counter.innerText = `${num} Cookies`
 
-    if (counter.innerText>5 && counter.innerText<25){
-        body.style.backgroundColor = "pink"
+    if (num>5 && num<25){
         btn.removeAttribute("hidden")
-    }else if(counter.innerText>20){
+    }else if(num>20){
         btn2.removeAttribute("hidden")
     }
 })
 
 btn.addEventListener("click", function(e){
-    counter.innerText = 0
-    btn.hasAttribute("hidden")
+    num = 0
+    counter.innerText = `${num} Cookies`
+    btn.setAttribute("hidden", true)
+    btn2.setAttribute("hidden", true)
 })
 
 btn2.addEventListener("click", function(e){
-    counter.innerText *=2
+    num *=2
+    counter.innerText = `${num} Cookies`
 })
